21ba966747c989c7e8f90aac073573d59a9c5dda
JugurtaO <jugurtaourzik@gmail.com>
2022-12-23 14:40:48 +0100

    +master: redesigned app

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,3 +1,7 @@
+
+
+//
+
 let button = document.getElementById('searchBtn');
 let input = document.getElementById('input');
 let carousel = document.getElementById("carouselExampleCaptions");
@@ -13,26 +17,58 @@ async function fetchAnimeAPI(event) {
     .then( response => {
       console.log(response); 
       let data = response.data;
-      for (let Anime of data) {
-        let main_div = document.createElement('div');
-        main_div.setAttribute("class", "carousel-item active");
-        let sub_div=document.createElement('div');
-        sub_div.setAttribute("class","carousel-caption d-none d-md-block")
-        // let img = document.createElement('img');
-        let h3=document.createElement('h3');
-        let p=document.createElement('p');
+      let i = 0;
+      for (i; i < data.length; i++) {
+        if (i === 0) {
+          let main_div = document.createElement('div');
+          main_div.setAttribute("class", "carousel-item active");
+
+          let sub_div = document.createElement('div');
+          sub_div.setAttribute("class", "carousel-caption d-none d-md-block")
+              
+          let img = document.createElement('img');
+          img.setAttribute('class', "d-block w-100");
+          img.setAttribute('src', `./${i}.jpg`);
+
+          let h3 = document.createElement('h3');
+          let p = document.createElement('p');
+          h3.textContent = `${data[i].anime}`;
+          p.textContent = `${data[i].quote}`;
+              
+          sub_div.appendChild(h3);
+          sub_div.appendChild(p);
+          main_div.appendChild(img);
+          main_div.appendChild(sub_div);
+
+          carousel2.appendChild(main_div);
+        }
+        else {
+          let main_div = document.createElement('div');
+          main_div.setAttribute("class", "carousel-item ");
+
+          let sub_div = document.createElement('div');
+          sub_div.setAttribute("class", "carousel-caption d-none d-md-block")
+              
+          let img = document.createElement('img');
+          img.setAttribute('class', "d-block w-100");
+          img.setAttribute('src', `./${i}.jpg`);
+
+          let h3 = document.createElement('h3');
+          let p = document.createElement('p');
+          h3.textContent = `${data[i].anime}`;
+          p.textContent = `${data[i].quote}`;
+              
+          sub_div.appendChild(h3);
+          sub_div.appendChild(p);
+          main_div.appendChild(img);
+          main_div.appendChild(sub_div);
+
+          carousel2.appendChild(main_div);
+          
+        }
         
-        
-        h3.textContent = `${ Anime.anime }`;
-        p.textContent = `${Anime.quote}`;
-        
-        sub_div.appendChild(h3);
-        sub_div.appendChild(p);
-        main_div.appendChild(sub_div);
-        
-        carousel2.appendChild(main_div);
       }
-      console.log(carousel);
+      
       //After rendering all data to the user browser , scroll down to the carousel
       carousel.scrollIntoView();
     })
